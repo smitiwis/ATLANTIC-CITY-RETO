6880bcfb0d94d6caf4e6fc00be3134e6d04952fe
smitiwis <sistemas.luismi@gmail.com>
2025-07-10 21:03:58 -0500

    Corregir render de detalles

--- src/pages/pokemon/Pokemon.tsx
+++ src/pages/pokemon/Pokemon.tsx
@@ -11,7 +11,7 @@ const Pokemon: React.FC = () => {
   const { loading, detailPokemon } = useDetailPokemon(id);
   console.log("Pokemon Detail:", detailPokemon);
 
-  if (loading) {
+  if (loading || !detailPokemon) {
     return (
       <div className="absolute top-1/2 transform -translate-y-1/2 text-center text-gray-500">
         Cargando detalles del Pokémon...
@@ -21,17 +21,17 @@ const Pokemon: React.FC = () => {
   return (
     <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
       <img
-        src={detailPokemon.sprites.back_default}
-        alt={detailPokemon.species.name}
+        src={detailPokemon?.sprites.back_default}
+        alt={detailPokemon?.species.name}
       />
-      <h1>Pokémon: {detailPokemon.forms[0].name}</h1>
+      <h1>Pokémon: {detailPokemon?.forms[0].name}</h1>
       <p>
-        <strong>Experiencia base:</strong> {detailPokemon.base_experience}
+        <strong>Experiencia base:</strong> {detailPokemon?.base_experience}
       </p>
 
       <h2>Habilidades</h2>
       <ul>
-        {detailPokemon.abilities.map((item: any, index: any) => (
+        {detailPokemon?.abilities.map((item: any, index: any) => (
           <li key={index}>
             {item.ability.name} {item.is_hidden ? "(oculta)" : ""}
           </li>
@@ -39,7 +39,7 @@ const Pokemon: React.FC = () => {
       </ul>
 
       <h2>Grito</h2>
-      <audio controls src={detailPokemon.cries.latest}>
+      <audio controls src={detailPokemon?.cries.latest}>
         Tu navegador no soporta el elemento de audio.
       </audio>
     </div>
